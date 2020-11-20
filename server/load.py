4c3189f040e3cf29369cd567b7f7dca3f84959ee
Quinten <tintin10q@hotmail.com>
2020-11-20 22:43:45 +0100

    Fixes for right communication with web server file in header for example

--- server/load.py
+++ server/load.py
@@ -2,8 +2,10 @@ from flask import Flask
 from .transcribe_blueprint import transcribe_blueprint
 
 
-app = Flask(__name__)
+app = application = Flask(__name__)
 app.register_blueprint(transcribe_blueprint)
+app.debug = True
+
 
 if __name__ == "__main__":
     app.run(debug=True)
